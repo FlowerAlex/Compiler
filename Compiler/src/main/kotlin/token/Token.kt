dc8c936e4ba9a03f6c4c05fe18b20909e8f65b69
aliaksei.liavonik@leancode.pl <aliaksei.liavonik@leancode.pl>
2021-12-13 01:13:05 +0100

    Complete Parser

--- Compiler/src/main/kotlin/token/Token.kt
+++ Compiler/src/main/kotlin/token/Token.kt
@@ -1,6 +1,6 @@
 package token
 
-class Token (val tokenType: TokenType,private val token: Any?,private val pos: Pair<Int, Int>){
+class Token (val tokenType: TokenType, val token: Any?, val pos: Pair<Int, Int>){
     override fun toString(): String {
         return token.toString() + ' ' + tokenType.toString() + ' ' + pos.toString()
     }
